d257930acf9460468413ecbbca7804cd75294796
SzymonKubica <szymo.kubica@gmail.com>
2023-10-14 11:31:43 +0100

    Added tests.

--- src/test/java/mips_emulator/AssemblerTest.java
+++ src/test/java/mips_emulator/AssemblerTest.java
@@ -1,11 +1,24 @@
 package mips_emulator;
 
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 class AssemblerTest {
 
+    private static final String testAssembly =
+            """ 
+                ADD R8, R6, R4
+                SW R5, 100(R6)
+                ADDI R4, R5, 50
+                BEQ R5, R7, 25""";
+
     @Test
-    void assembleLine() {
+    void assemble() {
+        String assembly = Assembler.assemble(testAssembly).stream().map(Objects::toString).collect(Collectors.joining("\n"));
+        Assertions.assertThat(assembly).isEqualTo(testAssembly);
     }
 }
